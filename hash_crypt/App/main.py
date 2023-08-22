dd6819a68df4663c643d75e466d0e77b62febc1f
LeonardoJunio <leonardo.lj.queiroz@gmail.com>
2023-08-22 03:55:24 -0300

    [UPD] Improved code structure

--- hash_crypt/App/main.py
+++ hash_crypt/App/main.py
@@ -4,6 +4,7 @@ import hashlib
 class FileConstants:
     PATH_TEXTS = '/files/texts'
     PATH_HASHS = '/files/hashs'
+    HASH_SUFFIX = 'hash'
 
 # 'object' would be the inheritance, don't need to make it explicit if it's this class
 class FileUtil(object):
@@ -27,61 +28,94 @@ class HashUtil:
     def generateTextHashMd5(texto: str):
         return hashlib.md5(str(texto).encode('utf-8')).hexdigest()
     
-class FileHelper:
-    def saveHashFile(arquivo, mensagemHash):
-        hashPath = FileUtil.dirFilesHash()
-        # salva o arquivo com a informacao do hash e o caminho
-        arquivoSaida = open((hashPath + '/' + arquivo + 'hash'), 'w')
-        arquivoSaida.write(mensagemHash)
-        arquivoSaida.close()
+class FileHelper:    
+    def saveMessageFile(path: str, file: str, text: str):
+        outputPath = path + '/' + file + FileConstants.HASH_SUFFIX
+        
+        try:
+            outputFile = open((outputPath), 'w')
+            
+            try:
+                outputFile.write(text)        
+            except:
+                print("Something went wrong when writing to the file.")
+            finally:
+                outputFile.close()        
+        except:
+            print("Something went wrong when opening the file") 
+    
+    def getAllTextFile(path: str) -> str:
+        if not path:
+            raise Exception("Path is empty") 
+        
+        file = open((path), 'r')
+        fileText = file.read()
+        file.close() 
+        
+        return fileText;
+    
+    def getLineTextFile(path: str) -> str:
+        if not path:
+            raise Exception("Path is empty") 
+        
+        file = open((path), 'r')
+        fileLineText = file.readline()
+        file.close() 
+        
+        return fileLineText;
     
 class HashHelper:
     def createHashFiles(path, file):
         if (file[-3::] == '.py'):  # not the code files
             return
 
-        if (file[-4::] == 'hash'):  # not the hash files
-            print('Arquivo ' + file + ' já consta o file hash')
+        if (file[-4::] == FileConstants.HASH_SUFFIX):  # not the hash files
+            print('File ' + file + ' is already a hash file.')
             return
 
-        diretorio = path+'/'+file  # path mais nome do file
-        arq = open((diretorio), 'r')
-        mensagem = arq.read()
+        pathFile = path + '/' + file
+        message = FileHelper.getAllTextFile(pathFile)        
+        pathFileMessage = pathFile + message
+        pathFileMessageHash = HashUtil.generateTextHashMd5(pathFileMessage)
 
-        texto = diretorio + mensagem
-        mensagemHash = HashUtil.generateTextHashMd5(texto)
-
-        FileHelper.saveHashFile(file, mensagemHash)
+        pathHashs = FileUtil.dirFilesHash()        
+        # saves the file with the hash information and the path
+        FileHelper.saveMessageFile(pathHashs, file, pathFileMessageHash)
     
-    def validateAllTextHash(nomeArquivo, pathTextOriginal):
+    def validateAllTextHash(file, pathFileMessage):
         pathHashs = FileUtil.dirFilesHash()
 
-        for caminhoH, diretoriosH, arquivosH in os.walk(pathHashs):
-            for arquivoH in arquivosH:
-                if (arquivoH != (nomeArquivo+'hash')):  # checking those that contain the hash data
+        for (pathHash, dirsHash, filesHash) in os.walk(pathHashs):
+            for fileHash in filesHash:
+                # checking those that contain the hash data
+                if (fileHash != (file + FileConstants.HASH_SUFFIX)):
                     continue
 
-                caminhototalH = caminhoH+'/'+arquivoH
-                arqH = open((caminhototalH), 'r')
-                mensagemArq = arqH.read()
-
-                if (mensagemArq == HashUtil.generateTextHashMd5(pathTextOriginal)):
-                    print('Arquivo ' + nomeArquivo + ' continua o mesmo.')
+                pathFileHash = pathHash + '/' + fileHash
+                originalMessage = FileHelper.getAllTextFile(pathFileHash)
+                
+                if (originalMessage == HashUtil.generateTextHashMd5(pathFileMessage)):
+                    print('File ' + file + ' remains the same.')
                 else:
-                    print('Arquivo ' + nomeArquivo + ' foi alterado.')
+                    print('File ' + file + ' has been changed.')
 
 class HashCrypt:
     def __init__(self):
         option:str = input('Please enter an option: (-i / -t / -x): ')
 
-        if (option == '-i'):
-            self.hashFiles()
-        elif (option == '-t'):
-            self.checkFiles()
-        elif (option == '-x'):
-            self.deleteHash()
-        else:
-            print('No valid option.')
+        try:
+            if (option == '-i'):
+                self.hashFiles()
+            elif (option == '-t'):
+                self.checkFiles()
+            elif (option == '-x'):
+                self.deleteHash()
+            else:
+                print('No valid option.')
+        except Exception as err:
+            print("Unexpected error:", err)
+            # print(f"Unexpected {err=}")
+            
         
     def hashFiles(self):
         pathTexts = FileUtil.dirFilesText()
@@ -91,33 +125,35 @@ class HashCrypt:
             for file in files:
                 HashHelper.createHashFiles(path, file)
 
-        print('Registro de guarda concluido.')
+        print('Guard registration completed.')
 
     def checkFiles(self):
         pathTexts = FileUtil.dirFilesText()
 
         for (path, dirs, files) in os.walk(pathTexts):
+            if not files:
+                print("There are no files.")
+                return
+  
             for file in files:
                 # check that it's not the hash files or the code
-                if (file[-4::] == 'hash' or file[-3::] == '.py'):
+                if (file[-4::] == FileConstants.HASH_SUFFIX or file[-3::] == '.py'):
                     continue
 
-                pathTotal = path+'/'+file
-                arq = open((pathTotal), 'r')
-                mensagemOri = arq.read()
-                nomeArquivo = file
-                pathTextOriginal = pathTotal + mensagemOri
+                pathFile = path + '/' + file
+                originalMessage = FileHelper.getAllTextFile(pathFile)
+                pathFileMessage = pathFile + originalMessage
 
-                HashHelper.validateAllTextHash(nomeArquivo, pathTextOriginal)
+                HashHelper.validateAllTextHash(file, pathFileMessage)
 
-        print('Verificação concluída.')
+        print('Check completed.')
 
     def deleteHash(self):  # removes the guard and hash files
         absolutePath = FileUtil.dirFilesHash()
 
         for (path, dirs, files) in os.walk(absolutePath):
             for file in files:
-                if (file[-4::] != 'hash'):
+                if (file[-4::] != FileConstants.HASH_SUFFIX):
                     continue
 
                 FileUtil.deleteFile(path + '/' + file)
