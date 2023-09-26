ec12b29cecbb0c18da954649ad9ecad646a5b824
LeonardoJunio <leonardo.lj.queiroz@gmail.com>
2023-09-26 05:46:19 -0300

    [UPD] Initial adjustments to the client code

--- chat_rc4_crypt/client/main.py
+++ chat_rc4_crypt/client/main.py
@@ -1,12 +1,13 @@
 """Script for Tkinter GUI chat client."""
+
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
-import tkinter 
+import tkinter
 import RC4
 import os
 
-#Ajuste para funcionamento no docker, quando não encontrar DISPLAY
-if os.environ.get('DISPLAY','') == '':
+# Adjustment for docker operation, when DISPLAY variable is not found
+if os.environ.get('DISPLAY', '') == '':
     print('no display found. Using :0.0')
     os.environ.__setitem__('DISPLAY', ':0.0')
 
@@ -16,114 +17,124 @@ tipoCrip = 1
 
 TEXT_PREFIX = "$$$"
 
+
+def clearInputField():
+    tkMsgStrVar.set("")
+
+
 def receive():
     """Handles receiving of messages."""
+
     while True:
         try:
-            msg = client_socket.recv(BUFSIZ).decode("utf8")
-            if(msg[0:3] == TEXT_PREFIX):
-                msg = msg.replace('$',"-")
-                # print(nome, flush=True)
-                msg_list.insert(tkinter.END, msg)
+            msg = socketClient.recv(BUFSIZE).decode("utf8")
+
+            if (msg[0:3] == TEXT_PREFIX):
+                msg = msg.replace('$', "-")
+                tkMsgList.insert(tkinter.END, msg)
             else:
-                msgDesencriptada = RC4.DesencriptarChat(msg)
-                msg_list.insert(tkinter.END, msgDesencriptada)
+                msgDecrypted = RC4.DesencriptarChat(msg)
+                tkMsgList.insert(tkinter.END, msgDecrypted)
         except OSError:  # Possibly client has left the chat.
             break
 
-def clearInputField():
-    my_msg.set("")
 
 def send(event=None):  # event is passed by binders.
     """Handles sending of messages."""
-    msg = my_msg.get().strip()
-    
+    msg = tkMsgStrVar.get().strip()
+
     if not msg:
         clearInputField()
         return
 
     global flagNome
     if (flagNome):
-        flagNome=False
-        
+        flagNome = False
+
         global nome
         nome = msg
     else:
-        if(msg != '{quit}' and msg[0:7] != "{chave}"):
+        if (msg != '{quit}' and msg[0:7] != "{chave}"):
             msg = nome + ': ' + msg
 
     clearInputField()
-    
-    if(msg[0:7] == "{chave}"):
-        if(msg[7]=='1'):
+
+    if (msg[0:7] == "{chave}"):
+        if (msg[7] == '1'):
             chave = msg[8:]
 
             print('Tipo: RC4')
-        
-            arquivoChave= open('key.txt', 'w')
+
+            arquivoChave = open('key.txt', 'w')
             arquivoChave.write(chave)
             arquivoChave.close()
-            
+
             msg = 'Chave alterada por ' + nome
-            
-            msgEncriptada = RC4.EncriptarChat(msg) 
-            client_socket.send(bytes(msgEncriptada, "utf8"))
-        elif(msg[8]=='2'): #7 ou 8?
+
+            msgEncriptada = RC4.EncriptarChat(msg)
+            socketClient.send(bytes(msgEncriptada, "utf8"))
+        elif (msg[8] == '2'):  # 7 ou 8?
             print('Tipo 2')
         else:
             print('tipo invalido')
-            
-    elif(msg == "{quit}"):
-        client_socket.send(bytes(msg, "utf8"))
-        client_socket.close()
-        top.quit()
-    else:   
-        msgEncriptada = RC4.EncriptarChat(msg) 
-        client_socket.send(bytes(msgEncriptada, "utf8"))
-            
+
+    elif (msg == "{quit}"):
+        socketClient.send(bytes(msg, "utf8"))
+        socketClient.close()
+        tkTop.quit()
+    else:
+        msgEncriptada = RC4.EncriptarChat(msg)
+        socketClient.send(bytes(msgEncriptada, "utf8"))
+
+
 def onClosing(event=None):
     """This function is to be called when the window is closed."""
-    
-    my_msg.set("{quit}")
+
+    tkMsgStrVar.set("{quit}")
     send()
 
-top = tkinter.Tk()
-top.title("Chat")
 
-messages_frame = tkinter.Frame(top)
-my_msg = tkinter.StringVar()  # For the messages to be sent.
-#my_msg.set("Type your messages here.")
-scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
+tkTop = tkinter.Tk()
+tkTop.title("Chat Python")
+
+tkMsgFrame = tkinter.Frame(tkTop)
+# For the messages to be sent
+tkMsgStrVar = tkinter.StringVar()
+# To navigate through past messages
+tkScrollbar = tkinter.Scrollbar(tkMsgFrame)
 # Following will contain the messages.
-msg_list = tkinter.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
-scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
-msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
-msg_list.pack()
-messages_frame.pack()
+tkMsgList = tkinter.Listbox(
+    tkMsgFrame,
+    height=15,
+    width=50,
+    yscrollcommand=tkScrollbar.set
+)
+tkScrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+tkMsgList.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+tkMsgList.pack()
+tkMsgFrame.pack()
 
-entry_field = tkinter.Entry(top, textvariable=my_msg)
-entry_field.bind("<Return>", send)
-entry_field.pack()
-send_button = tkinter.Button(top, text="Send", command=send)
-send_button.pack()
+tkMsgEntryField = tkinter.Entry(tkTop, textvariable=tkMsgStrVar)
+tkMsgEntryField.bind("<Return>", send)
+tkMsgEntryField.pack()
+tkSendButton = tkinter.Button(tkTop, text="Send", command=send)
+tkSendButton.pack()
 
-top.protocol("WM_DELETE_WINDOW", onClosing)
+tkTop.protocol("WM_DELETE_WINDOW", onClosing)
 
 HOST = '0.0.0.0'
 PORT = 5354
+BUFSIZE = 1024
 
-BUFSIZ = 1024
-ADDR = (HOST, PORT)
+socketClient = socket(AF_INET, SOCK_STREAM)
+socketClient.connect((HOST, PORT))
 
-client_socket = socket(AF_INET, SOCK_STREAM)
-client_socket.connect(ADDR)
-
-receive_thread = Thread(target=receive)
-# A thread that runs in the background, and is not expected to complete its execution before the program exits. 
-# On the other hand, non-daemon threads are critical to the functioning of the program, 
+threadReceive = Thread(target=receive)
+# A thread that runs in the background, and is not expected to complete its execution before the program exits.
+# On the other hand, non-daemon threads are critical to the functioning of the program,
 # and they prevent the main program from exiting until they have completed their execution
 # The Daemon Thread does not block the main thread from exiting and continues to run in the background.
-receive_thread.daemon = True
-receive_thread.start()
+threadReceive.daemon = True
+threadReceive.start()
 # Starts GUI execution
-tkinter.mainloop() 
+tkinter.mainloop()
